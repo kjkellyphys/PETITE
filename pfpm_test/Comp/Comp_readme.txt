b2690c71e2aaaee752e472567192c955560cacb4
Pedro Machado <45109897+pmachado85@users.noreply.github.com>
2023-07-17 19:41:09 -0500

    reformulate adaptive_maps
    reformulated the directory structure for the adaptive maps. The used only gives the mother directory, all subdirectories are hard coded in PETITE. Only adaptive maps are saved throughout the generation process.

--- pfpm_test/Comp/Comp_readme.txt
+++ pfpm_test/Comp/Comp_readme.txt
@@ -4,4 +4,4 @@ Energy/GeV |  Filename
 
     0.010  |    0.p
   100.000  |    1.p
-Integrators made on 2023-07-17 17:31:54.410585
+Integrators made on 2023-07-17 19:37:53.055574
--- utilities/find_maxes.py
+++ utilities/find_maxes.py
@@ -24,7 +24,7 @@ from datetime import datetime
 import vegas
 from tqdm import tqdm
 
-#Dictionary of proceses with corresponding x-secs, form factors and Q**2 functions
+#D ictionary of proceses with corresponding x-secs, form factors and Q**2 functions
 process_info ={    'PairProd' : {'diff_xsection': dsigma_pairprod_dimensionless,   'form_factor': g2_elastic, 'QSq_func': pair_production_q_sq_dimensionless},
                    'Comp'     : {'diff_xsection': dsigma_compton_dCT,     'form_factor': unity,      'QSq_func': dummy},
                     'Brem'     : {'diff_xsection': dsigma_brem_dimensionless,       'form_factor': g2_elastic, 'QSq_func': brem_q_sq_dimensionless},
@@ -33,9 +33,15 @@ process_info ={    'PairProd' : {'diff_xsection': dsigma_pairprod_dimensionless,
                     'Bhabha'   : {'diff_xsection': dsigma_bhabha_dCT, 'form_factor': unity,      'QSq_func': dummy}}
 
 def get_file_names(path):
+    ''' Get the names of all the files in a directory.
+    Input:
+        path: path to the directory
+    Output:
+        pickle_files: list of names of all the .p files in the directory (VEGAS integrator adaptive maps for given process and given energy)
+        readme_file: name of the readme file in the directory (contains information about the process and the energy)
+    '''
     all_files = os.listdir(path)
-    print('files:', all_files)
-    #pickle_files = [file for file in all_files if file.endswith(".p")]
+    # print('files:', all_files)
     pickle_files = [file for file in all_files if file.endswith(".p")]
     if 'readme.txt' in all_files:
         readme_file = 'readme.txt'
@@ -50,7 +56,12 @@ def do_find_max_work(params, process_file):
         params: dictionary of parameters for the process
         process_file: array of event_info and integrand which was read from a file (0.p, 1.p, ...)
     Output:
-        samp_dict: dictionary of information about the sampling, containing the number of evaluations used in VEGAS (neval), the maximum value of the integrand (max_F) and the adaptive map used in the sampling (adaptive_map); and possibly the minimum energy of the outgoing photon (Eg_min) and electron (Ee_min) if they were specified in the event_info
+        samp_dict: dictionary of information about the sampling, containing:
+            'neval': number of evaluations used in VEGAS (neval)
+            'max_F': maximum value of the integrand (max_F)
+            'Eg_min': minimum energy of the outgoing photon (Eg_min) if it was specified in the event_info
+            'Ee_min': minimum energy of the outgoing electron (Ee_min) if it was specified in the event_info
+            'adaptive_map': adaptive map used in the sampling (adaptive_map)
         xSec: dictionary of cross sections for each target material
         event_info['E_inc']: energy of the incoming particle
     """
@@ -104,10 +115,24 @@ def do_find_max_work(params, process_file):
     return(samp_dict, xSec, event_info['E_inc'])
 
 def main(params):
-    """ Find the maximum value of the integrand for a given process_file.
+    """ Find the maximum value of the integrand for a given process by looking into the directory containing the adaptive maps.
     Input:
-        params: dictionary of parameters for the process
+        params: dictionary of parameters for the process containing the following keys:
+            'process': list of processes to be run
+            'Z_T': list of target materials to be run
+            'neval': number of evaluations used in VEGAS
+            'save_location': path to the mother directory whose subdirs contain the adaptive maps
+            # add all needed parameters here!
     """
+    '''
+    Pseudoalgorithm:
+    Loops over all processes in params['process']. 
+    Access each 'process' directory containing the 'process_AdaptiveMaps.npy', and load these files.
+    Runs do_find_max_work function on each of these files for each energy. 
+    All the results are saved in a single file as a dictionary and pickled (sm_maps.pkl).
+    *Later I will create the xsec dictionary and save it in a separate file (sm_xsec.pkl).
+    '''
+
     # params['process'] should be a list of all processes to be run
     # available processes are ['PairProd', 'Comp', 'Ann', 'Brem', 'Moller', 'Bhabha']
     # If a single process is given, convert single process to a list if necessary
@@ -116,95 +141,53 @@ def main(params):
     # Convert single target to a list if necessary
     if isinstance(params['Z_T'], int):
         params['Z_T'] = [params['Z_T']]
-
     print('List of processes: ', params['process'])
+    print('List of atomic number of target materials: ', params['Z_T'])
 
-    # This needs to loop over all processes in params['process']. Then, acess each directory, loads the 'process_AdaptiveMaps.npy' file, and then runs the do_find_max_work function on each of these files. Finally, all the results are saved in a dictionary and pickled (sm_maps.pkl).
-
-    #Set up processes to run
-    path = "../" + params['import_directory'] + "/" + params['process'][0] + "/"
-    file_list, readme_file = get_file_names(path)
-    print('Files to process: ', file_list)
-    #print readme for info purposes
-    if not(readme_file==0):
-        print("Files to process")
-        with open(path + readme_file, 'r') as file_temp:
-            print(file_temp.read())
-
-    # Initialise dictionaries to store samples and cross sections
-    samp_dict = {}
-    xSec_dict = {}
+    # Loop over all processes
     for process in params['process']:
-        samp_dict[process] = []
-        xSec_dict[process] = {}
-
+        # Get the path to the directory containing the adaptive maps
+        path = params['save_location'] + '/' + process + '/'
+        # Get adaptive map main file (created by stitch_integrators)
+        adaptive_maps_file = path + process + '_AdaptiveMaps.npy'
+        # Load adaptive map file. Format: list of [params, adaptive_map]
+        adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
+        # Initialise dictionaries to store samples and cross sections
+        final_sampling_dict = {}
+        final_xsec_dict = {}
+        final_sampling_dict[process] = []
+        final_xsec_dict[process] = {}
         # Initialise dictionary to store cross sections for each target material
         for ZT in params['Z_T']:
-            xSec_dict[process][ZT] = []
+            final_xsec_dict[process][ZT] = []
+        print('File being processed: ', adaptive_maps_file)
+        # Add relevant info to params
+        params['process']   = process
+        params['diff_xsec'] = process_info[process]['diff_xsection']
+        params['FF_func']   = process_info[process]['form_factor']
+        params['QSq_func']  = process_info[process]['QSq_func']
 
-        for file in file_list: # we can just run over all rows in the process_AdaptiveMaps.npy file instead of running through the #.p files
-            print('File being processed: ', file)
-            # loading file content
-            process_file = np.load(path + file, allow_pickle=True)
-            # for process_file in tqdm(params_and_vegas_obj):
-            #     print('process file: ',process_file)
-            #     input() 
-            #     for process in params['process']:
-            if process_file[0]['process'] == process:
-                process_params = copy.deepcopy(params)
-                process_params['process'] = process
-                process_params['diff_xsec'] = process_info[process]['diff_xsection']
-                process_params['FF_func'] = process_info[process]['form_factor']
-                process_params['QSq_func'] = process_info[process]['QSq_func']
+        ####=====------ FIND MAX ------=====####
+        # loop over all adaptive_map in adaptive_maps
+        for adaptive_map in adaptive_maps:
+            sampling, cross_section, incoming_energy = do_find_max_work(params, adaptive_map)
+            # Append sampling dictionary to final sampling dictionary
+            final_sampling_dict[process].append(sampling)
+            # Append incoming energy and cross section dictionary to final cross section dictionary for each target material
+            for ZT in params['Z_T']:
+                final_xsec_dict[process][ZT].append([incoming_energy, cross_section[ZT]])
+        ####=====------ FIND MAX ------=====####
 
-                ###########################################################################################
-                # Find max work for each process
-                samp_dict_TEMP, xSec_dict_TEMP, energy_TEMP = do_find_max_work(process_params, process_file)
-                samp_dict[process].append(samp_dict_TEMP)
-                for ZT in params['Z_T']:
-                    xSec_dict[process][ZT].append([energy_TEMP, xSec_dict_TEMP[ZT]])
-                # done
-                ###########################################################################################
-
-                
-    save_path = "../" + params['save_location']  
-    if os.path.exists(save_path) == False:
-            os.system("mkdir -p " + save_path)
-    
-    ### Do we need the following code?
-    try:
-        f_xSecs = open(save_path + "/sm_xsecs.pkl","rb")
-        xSec_dict_existing = pickle.load(f_xSecs)
-    except:
-        xSec_dict_existing = {}
-    try:
-        f_samps = open(save_path + "/sm_maps.pkl","rb")
-        samp_dict_existing = pickle.load(f_samps)
-    except:
-        samp_dict_existing = {}
-
-    for process in params['process']:
-        if process in xSec_dict_existing.keys():
-            print("Overwriting existing cross section for process " + process)
-        xSec_dict_existing[process] = xSec_dict[process]
-        if process in samp_dict_existing.keys():
-            print("Overwriting existing samples for process " + process)
-        samp_dict_existing[process] = np.array(samp_dict[process])
-    ### end of code that may not be needed
-
-    f_xSecs_save = open(save_path + "/sm_xsecs.pkl","wb")
-    f_samps_save = open(save_path + "/sm_maps.pkl","wb")
-    pickle.dump(xSec_dict_existing,f_xSecs_save)
-    pickle.dump(samp_dict_existing,f_samps_save)
-    f_xSecs_save.close()
-    f_samps_save.close()
-    # print out file path
-    print("Saved cross sections to " + save_path + "/sm_xsecs.pkl")
-    print("Saved samples to " + save_path + "/sm_maps.pkl")
-    return()
+        # Save sampling and cross section dictionaries to mother directory
+        with open(params['save_location'] + '/sm_maps.pkl', 'wb') as f:
+            pickle.dump(final_sampling_dict, f)
+        with open(params['save_location'] + '/sm_xsec.pkl', 'wb') as f:
+            pickle.dump(final_xsec_dict, f)
+    print("Saved cross sections to " + params['save_location'] + "/sm_xsecs.pkl")
+    print("Saved samples to " + params['save_location'] + "/sm_maps.pkl")
+    return
 
 
-###################################################################
 
 startTime = datetime.now()
 
