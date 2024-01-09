db3afa7433f52806fe327cbe6403ddd063cc3f0d
Kevin Kelly <kkelly942@gmail.com>
2024-01-09 13:25:33 -0600

    v1

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ import setuptools
 setuptools.setup(
     name='PETITE',
     package_dir={'PETITE':'src'},
-    version="0.0.0",
-    author ="Kevin J. Kelly",
-    description="Packages...",
+    version="1.0.0",
+    author ="Nikita Blinov, Patrick J. Fox, Kevin J. Kelly, Pedro A.N. Machado, Ryan Plestid",
+    description="Package for Electromagnetic Transitions in Thick-target Environments\
+                 Allows for simulations of standard model electromagnetic showers and\
+                 production of new-physics particles in beam-dump environments.",
     packages=["PETITE"]
 )
