cc6ea7e295c80f5b52f384b532dce3389db197be
Kevin Kelly <kkelly942@gmail.com>
2024-01-17 11:13:11 -0600

    More complete setup.py
    Thanks to Stephan Meighen-Berger for suggestions.

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-import setuptools
+#!/usr/bin/env python
+import pathlib
+from setuptools import setup
 
-setuptools.setup(
+# Parent directory
+HERE = pathlib.Path(__file__).parent
+
+# The readme file
+README = (HERE / "README.md").read_text()
+
+setup(
     name='PETITE',
     package_dir={'PETITE':'src'},
     version="1.0.0",
     author ="Nikita Blinov, Patrick J. Fox, Kevin J. Kelly, Pedro A.N. Machado, Ryan Plestid",
+    author_email="nblinov@yorku.ca, pjfox@fnal.gov, kjkelly@tamu.edu, pmachado@fnal.gov, rplestid@caltech.edu",
     description="Package for Electromagnetic Transitions in Thick-target Environments\
                  Allows for simulations of standard model electromagnetic showers and\
                  production of new-physics particles in beam-dump environments.",
-    packages=["PETITE"]
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kjkellyphys/PETITE",
+    install_requires=[
+        "vegas>=5.4.2",
+        "tqdm",
+        "numpy==1.24"
+    ],
+    extras_require={
+        "interactive": ["nbstripout", "matplotlib", "jupyter"]
+    },
+    packages=["PETITE"],
 )
