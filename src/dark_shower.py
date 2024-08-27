Numerical fixes

src/dark_shower.py
@@ -213,8 +213,8 @@ class DarkShower(Shower):
         return self._dark_brem_cross_section 
     def set_DarkAnnXSec(self):
         """ Returns array of [energy,cross-section] values for e+e- annihilation with a bound electron """ 
-        E_min = np.max([self._minimum_calculable_energy[-11], 0.001*self._resonant_annihilation_energy])
-        E_max = np.min([1000*self._resonant_annihilation_energy, self._dark_brem_cross_section[-1][0]])
+        E_min = np.max([self._minimum_calculable_energy[-11], 0.001*self._resonant_annihilation_energy])       
+        E_max = self._dark_brem_cross_section[-1][0]
         energy_list = np.logspace(np.log10(E_min), np.log10(E_max), 200)
         #energy_list = np.logspace(np.log10(0.0016), np.log10(1000), 200)
         #ER0 = ((self._mV**2 - 2*m_electron**2)/(2*m_electron))

utilities/find_maxes.py
@@ -160,8 +160,12 @@ def main(params):
         # Get adaptive map main file (created by stitch_integrators)
         adaptive_maps_file = path + process + '_AdaptiveMaps.npy'
         # Load adaptive map file. Format: list of [params, adaptive_map]
-        adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
-
+        try:
+            adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
+        except:
+            path = params['save_location'] + process + '/'
+            adaptive_maps_file = path + process + '_AdaptiveMaps.npy'
+            adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
         final_sampling_dict[process] = []
         final_xsec_dict[process] = {}
         # Initialise dictionary to store cross sections for each target material
@@ -249,8 +253,12 @@ def main_dark(params):
             path = params['save_location'] + '/auxiliary/' + process + '/mV_' + str(int(1000.*mV)) + "MeV/"
             adaptive_maps_file = path + process + '_AdaptiveMaps.npy'
             # Load adaptive map file. Format: list of [params, adaptive_map]
-            adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
-
+            try:
+                adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
+            except:
+                path = params['save_location'] + process + '/mV_' + str(int(1000.*mV)) + "MeV/"
+                adaptive_maps_file = path + process + '_AdaptiveMaps.npy'
+                adaptive_maps = np.load(adaptive_maps_file, allow_pickle=True)
             final_sampling_dict[mV][process] = []
             final_xsec_dict[mV][process] = {}
             # Initialise dictionary to store cross sections for each target material