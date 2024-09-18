Correcting factor of 2 in cross section

src/atomic_compton.py
@@ -101,4 +101,4 @@ def sigma_atomic_comp(k,mV, Zeff):
     
     beta = 2*alpha_em/np.pi*(np.log(s/m_e**2) - 1.0)
 
-    return( (4.0*np.pi*alpha_em)*((mV**2+2*m_e**2)* (2/3/Lambda/m_e)*(1/k**2)*(beta/2)*combine(a,b)) )
+    return( (4.0*np.pi*alpha_em)*((mV**2+2*m_e**2)* (2/3/Lambda/m_e)*(1/k**2)*(beta/4)*combine(a,b)) )